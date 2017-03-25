d1b3d6b5ecd5476d35cda759ef309c22f693a426
Guheyhey <joeglancith@hotmail.com>
2017-03-24 19:06:25 -0700

    added

---
+++ app/src/main/java/com/example/joegl/myezresume/model/Education.java
@@ -0,0 +1,24 @@
+package com.example.joegl.myezresume.model;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by joegl on 2017/3/24.
+ */
+
+public class Education {
+
+    public String id;
+
+    public String school;
+
+    public String major;
+
+    public Date startDate;
+
+    public Date endDate;
+
+    public List<String> courses;
+
+}
